2831219fb3c2f3e0d4cd63f03598ca10cea3dc32
Shivam Gupta <78139495+revmag@users.noreply.github.com>
2024-07-09 13:41:49 +0530

    Update run_pipeline.py

--- pipeline/run_pipeline.py
+++ pipeline/run_pipeline.py
@@ -197,7 +197,7 @@ def generate_and_save_candidate_directions(
 def normalize_vector(vector):
     """Normalize a given vector to have unit length."""
     norm = torch.linalg.vector_norm(vector)
-    if th.isclose(norm, th.tensor(0.)):
+    if torch.isclose(norm, torch.tensor(0.)):
         return vector
     return vector / norm
 
